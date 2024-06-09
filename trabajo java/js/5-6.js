5de34d25c37b84dd35f061e08edbbc9d9876248b
LeoChalupp <leoezequielchalupp@gmail.com>
2024-06-09 19:10:41 -0300

    Add files via upload

---
+++ trabajo java/js/5-6.js
@@ -0,0 +1,22 @@
+/* 1 */
+let apellido = 'Martinez'
+
+prompt('apellido')
+
+/* 2 */
+console.log(`El apellido tiene ${apellido.length} caracteres`)
+
+/* 3 */
+console.log(apellido.at(-1))
+
+/* Tarea 2*/
+
+let nombre='julian'
+
+console.log(nombre.toUpperCase())
+
+/* 2 */
+
+let nombreCapitalizado=(nombre.at(0).toUpperCase())
+let restoNombre=(nombre.substring(1))
+console.log(`${nombreCapitalizado}${restoNombre}`)
